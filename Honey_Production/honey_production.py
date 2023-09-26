3ee961a81b536f5fcac936bd3204cf0100a69518
Alvarro123 <t.skrzypczak.pl@gmail.com>
2023-09-26 18:46:13 +0200

    With Graph

--- Honey_Production/honey_production.py
+++ Honey_Production/honey_production.py
@@ -1,6 +1,16 @@
 from sklearn import linear_model
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 dataset = pd.read_csv("US_honey_dataset_updated.csv")
-print(dataset.head())
-dataset.info()
+#print(dataset.head())
+#dataset.info()
+prod_year = dataset.groupby("year").agg({"value_of_production":"mean"}).reset_index()
+#print(prod_year.head())
+X = prod_year["year"]
+y = prod_year["value_of_production"]
+plt.plot(X,y)
+plt.xlabel("Years")
+plt.ylabel("Honey Mean Volume")
+plt.show()
+

375e0e0132bacc234b98a26cf9407a3d475dc7cd
Alvarro123 <t.skrzypczak.pl@gmail.com>
2023-09-30 23:45:06 +0200

    new

---
+++ Stock_Predictor/Stock_Predictor.py
@@ -0,0 +1,4 @@
+class Stooq:
+    def build_training_dataset(self,url):
+        #tutaj zacząć od tego e tworzy folder o nazwie dataset we wskazanej lokalizacji i zaczyna pobierac dane ze stooq po kolei jak leci ze strony
+        pass
